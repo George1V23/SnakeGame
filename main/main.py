0f91fb5cf717c0e6cbf0b98ba8d67585d7d0fbae
George1V23 <vidinei.george@gmail.com>
2026-08-26 09:45:49 +0300

    Snake head wrapping around window

--- main/main.py
+++ main/main.py
@@ -1,15 +1,15 @@
+import os
+import random
 import kivy
-
-kivy.require('1.9.0')  # this restricts the kivy version
 
 from kivy.app import App  # base Class of App inherits from the App class
 from kivy.lang import Builder  # allows loading in Kv Design language regardless filename
+from kivy.properties import NumericProperty, ListProperty
 from kivy.uix.widget import Widget  # elements of a graphical user interface that form part of the User Experience
 from kivy.uix.relativelayout import RelativeLayout  # allows setting relative coordinates for children
 from kivy.core.window import Window  # used to get keyboard input
-from kivy.graphics import Rectangle
 
-kv = Builder.load_file("drawing.kv")  # load kv file
+kv = Builder.load_file(os.path.join(os.path.dirname(__file__), "drawing.kv"))  # load kv file
 
 
 # Create the Widget class
@@ -19,20 +19,27 @@ class PaintBrush(Widget):
 
 # Create a square for drawing
 class Square(Widget):
-    #canvas = Rectangle(pos=(0, 0), size=(50, 50))
-    #rect.Color(1, 1, 1)
     pass
 
+
+# Create a triangle for drawing
+class Triangle(Widget):
+    angle = NumericProperty(0)
+    color = ListProperty([1, 1, 1])
 
 
 # Layout class where PaintBrush() class is drawn
 class Drawing(RelativeLayout):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-        #self.keyboard = Window.request_keyboard(self.on_keyboard_closed, self)
-        #self.keyboard.bind(on_key_down=self.on_key_down)
         Window.bind(on_key_down=self.on_key_down)
+
+        self.triangle = Triangle()
+        max_x = max(0, int(Window.width - self.triangle.width))
+        max_y = max(0, int(Window.height - self.triangle.height))
+        self.triangle.pos = (random.randint(0, max_x), random.randint(0, max_y))
+        self.add_widget(self.triangle)
 
     # On mouse press how PaintBrush behave
     def on_touch_down(self, touch):
@@ -40,6 +47,7 @@ class Drawing(RelativeLayout):
         pb.center = touch.pos
         self.add_widget(pb)
         re = Square()
+        re.center = touch.pos
         self.add_widget(re)
 
     # On mouse movement how PaintBrush behave
@@ -49,28 +57,34 @@ class Drawing(RelativeLayout):
         self.add_widget(pb)
 
     def on_keyboard_closed(self):
-        #re = Square()
-        #self.add_widget(re)
-        #Rectangle(pos=(0, 0), size=(50, 50))
         pass
 
     def on_key_down(self, *args):
         move = keyboard_handler(args[0], args[1], args[2], args[3], args[4])
         print(move)
-        # re = Square()
-        # self.add_widget(re)
-        pass
+        if move == (0, 1):
+            self.triangle.angle = 0
+        elif move == (-1, 0):
+            self.triangle.angle = 90
+        elif move == (0, -1):
+            self.triangle.angle = 180
+        elif move == (1, 0):
+            self.triangle.angle = 270
+
+        step = 10
+        self.triangle.x = (self.triangle.x + move[0] * step) % Window.width
+        self.triangle.y = (self.triangle.y + move[1] * step) % Window.height
 
 
 def keyboard_handler(instance, key, scancode, codepoint, modifiers):
     print("key event: %s" % [instance, key, scancode, codepoint, modifiers])
-    if codepoint == 'w' or key == '273':
+    if codepoint in ('w', 'W') or key in (273, '273'):
         return (0, 1)
-    elif codepoint == 'a' or key == '276':
+    elif codepoint in ('a', 'A') or key in (276, '276'):
         return (-1, 0)
-    elif codepoint == 's' or key == '274':
+    elif codepoint in ('s', 'S') or key in (274, '274'):
         return (0, -1)
-    elif codepoint == 'd' or key == '275':
+    elif codepoint in ('d', 'D') or key in (275, '275'):
         return (1, 0)
     return (0, 0)
 
@@ -79,7 +93,6 @@ def keyboard_handler(instance, key, scancode, codepoint, modifiers):
 class SnakeApp(App):
 
     def build(self):
-        #Window.bind(on_key_down=keyboard_handler)
         return Drawing()
 
 
